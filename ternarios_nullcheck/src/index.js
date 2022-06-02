8fdbba81fbb6d860f5422e6577485b24c8126218
Marcos Sanchez - Desarrollo <msanchez@hircasa.com>
2022-06-02 17:59:17 -0500

    ** Concluida la introduccion a JS **

---
+++ ternarios_nullcheck/src/index.js
@@ -0,0 +1,10 @@
+let primerNombre;
+let segundoNombre;
+
+console.log(`${primerNombre || "NO hay primer nombre "} ${segundoNombre}`);
+
+let isActive = false;
+
+const message = isActive ? "Esta activo" : "Esta Inactivo";
+
+console.log(message);
